bcffd399c6e3b7b723167a511ddd318c2d5fd91f
QuocAnVN1310 <106017811+QuocAnVN1310@users.noreply.github.com>
2022-11-06 07:31:43 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,15 +3,15 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By Limted" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user githubuser 2#3RmQh49yB!kJq25RzhCC /add >nul
-net localgroup administrators githubuser /add >nul
-net user githubuser /active:yes >nul
+net user QuocAnVN @DEV3.QYTB /add >nul
+net localgroup administrators QuocAnVN /add >nul
+net user QuocAnVN /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant githubuser:F >nul
-ICACLS C:\Windows\installer /grant githubuser:F >nul
+ICACLS C:\Windows\Temp /grant QuocAnVN:F >nul
+ICACLS C:\Windows\installer /grant QuocAnVN:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
